c8192819eda270550f0948245ffa1f1b30b6195a
Maria Brand <maria.brand@uao.edu.co>
2022-05-15 19:14:17 -0500

    generar objetos random en posiciones determinadas

---
+++ Assets/Scripts/MonoBehaviours/Spawner.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> spawnSpots;
+    public List<GameObject> objetos;
+    public int startWait;
+    public float spawnWait, spawnMostWait, spawnLeastWait;
+    public bool stop;
+    int randEnemy, randPosition;
+    
+
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnSpots.Count == 0 ) {
+            stop = true;
+        }
+        else {
+            spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+        }
+        
+    }
+
+    IEnumerator waitSpawner()
+    {
+
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            randEnemy = Random.Range(0, objetos.Count - 1);
+            randPosition = Random.Range(0, spawnSpots.Count - 1);
+
+            Instantiate(objetos[randEnemy], spawnSpots[randPosition].position, Quaternion.identity);
+
+            spawnSpots.Remove(spawnSpots[randPosition]);
+
+            
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+
+
+    }
+}
